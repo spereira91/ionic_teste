f7533f045357b1cdfd84fbf1fa82f942e209a3be
lauraTchaly <lauramanes@outlook.com>
2023-02-07 13:34:04 -0300

    PRIMEIRAS AULAS DE IONIC

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./page/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'form',
+    loadChildren: () => import('./page/form/form.module').then( m => m.FormPageModule)
+  },
+  {
+    path: 'contato',
+    loadChildren: () => import('./page/contato/contato.module').then( m => m.ContatoPageModule)
+  },
+  {
+    path: 'sobre',
+    loadChildren: () => import('./page/sobre/sobre.module').then( m => m.SobrePageModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/page/form/form.page.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { forceUpdate } from 'ionicons/dist/types/stencil-public-runtime';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.page.html',
+  styleUrls: ['./form.page.scss'],
+})
+export class FormPage implements OnInit {
+
+  constructor(   
+    
+    private activateRouter: ActivatedRoute,
+    private router: Router,
+
+    ) { 
+  }
+  routeId = null; //Variável que guarda a rota
+  produto: any = {};
+
+  ngOnInit() {
+  this.routeId=
+  this.activateRouter.snapshot.params['id'];
+
+  if(this.routeId){
+    // Se o id do produto for encontrado
+    // Ativa o banco de dados
+
+    this.banco.getOneItem(this.routeId).subscribe(caixa => {this.produto = caixa});]
+
+  }
+
+  Update(form: any){
+
+    this.banco.updateItem(form.value, this.routeId);
+    this.router.navigate[''];
+    this.util.tostando("Item Atualizado com sucesso","middle", 2000, "medium");
+
+  }
+  declarations: [FormPage]
+
+  }
+
+}
