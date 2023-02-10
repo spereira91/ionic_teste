2b93da26178532f0d3e39c1838bd682af0ab0911
lauraTchaly <lauramanes@outlook.com>
2023-02-10 16:43:14 -0300

    MODIFICANDO DATABASE

--- src/app/dataBase/database.service.ts
+++ src/app/dataBase/database.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+ import { Produto } from '../model/produto';
+import { identity } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  HttpOptions = {
+
+    headers: new HttpHeaders({'content-Type' : 'applications/json'})
+  }
+// API é o nome da variável que será só de leitura(READONLY)
+readonly API = "http://localhost:3000/lista";
+
+getItem(){
+  return this.http.get<Produto[]>(this.API);
+}
+
+// MÉTODO TRAZER UM UNICO ITEM
+getOneItem(id: number){
+
+   return this.http.get<Produto>(this.API + id);
+
+}
+
+// MÉTODO PARA APAGAR UM ITEM
+delItem(id:number){
+
+  return this.http.delete(this.API + id).subscribe()
+
+}
+
+// MÉTODO DE ALTERAÇÃO DE STATUS
+statusItem(item: Produto){
+  return this.http.put(this.API + item.id,JSON.stringify(item),
+  this.HttpOptions).subscribe();
+}
+
+// MÉTODOS PARA ATUALIZAR OS DADOS
+UpdateItem(id: any, item: Produto){
+  return this.http.put(this.API + id, 
+  JSON.stringify(item),
+  this.HttpOptions).subscribe()
+}
+
 }
